97193e7c7a1f1c9ad048e1ff3eeedf225c3cb4a0
Pragyan <youremail@example.com>
2025-02-25 20:04:17 +0530

    feat: authorization added with Bearer token

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { getUsers , getUser } from "../controller/user.controller.js";
+import authorize from "../middleware/auth.middleware.js";
+
 
 const userRouter = Router();
 
 userRouter.get('/' , getUsers);
 
-userRouter.get('/:id' , getUser);
+userRouter.get('/:id' , authorize , getUser);
 
 userRouter.post('/' , (req,res) => res.send({title:'CREATE a user'}));
 
